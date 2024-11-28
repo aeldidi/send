f21eebea32d373e9ad45141c89f3ee5e675ae241
Ayman El Didi <ayman@eldidi.org>
2024-11-27 23:18:47 -0700

    some work

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 use axum::{
     body::Body,
-    extract::{
-        multipart::Field,
-        ws::{Message, WebSocket},
-        Multipart, WebSocketUpgrade,
-    },
-    http::StatusCode,
+    extract::Multipart,
+    http::{HeaderMap, StatusCode},
     response::Response,
     routing::post,
     Router,
 };
 use chrono::Duration;
 use clap::Parser;
-use config;
-use dynerror::{bail, Context, Error, Result};
+use dynerror::{bail, Context, Error};
 use s3::{creds::Credentials, Bucket, Region};
 use serde::{Deserialize, Serialize};
 use std::{env, path::Path, sync::Arc};
 use tokio::{io, net::TcpListener, sync::OnceCell};
 use tokio_util::io::StreamReader;
-use tracing::error;
+use tracing::{error, info};
+use tracing_subscriber::{
+    layer::SubscriberExt, util::SubscriberInitExt, EnvFilter,
+};
 
 mod bytes_base64;
 mod duration_positive_seconds;
@@ -47,6 +45,7 @@ struct Config {
     s3_region: String,
     upload_buffer_size: i32, // Minimum size of 5 MiB
     upload_size_limit: i32,
+    listen_addr: String,
 }
 
 static CONFIG: OnceCell<Config> = OnceCell::const_new();
@@ -58,20 +57,20 @@ struct Args {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
     let args = Args::parse();
     let config_parser = config::Config::builder()
         .add_source(config::File::with_name(&args.config_path))
         .add_source(config::Environment::with_prefix("SEND"))
         .build()
-        .unwrap();
-    let config = match config_parser.try_deserialize::<Config>() {
-        Ok(x) => x,
-        Err(err) => {
-            eprintln!("failed to parse {}: {}", args.config_path, err);
-            return;
-        }
-    };
+        .context("error creating config parser")?;
+    let config = config_parser
+        .try_deserialize::<Config>()
+        .context("error parsing config")?;
     BUCKET
         .set(
             s3::Bucket::new(
@@ -84,82 +83,106 @@ async fn main() {
                     None,
                     None,
                 )
-                .unwrap(),
+                .context("error initializing S3 credentials")?,
             )
-            .unwrap(),
+            .context("error initializing S3 bucket")?,
         )
-        .unwrap();
-    CONFIG.set(config).unwrap();
+        .context("error setting global bucket")?;
+    CONFIG.set(config).context("error setting config");
 
     let app = Router::new().route("/v1/upload", post(upload));
 
-    let listener = TcpListener::bind(":13370").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = TcpListener::bind(config.listen_addr)
+        .await
+        .context("couldn't bind to listen_addr")?;
+    info!("listening on {}", config.listen_addr);
+    axum::serve(listener, app)
+        .await
+        .context("error serving HTTP")
 }
 
-async fn upload(mut mp: Multipart) -> Result<Response> {
+async fn upload(
+    headers: HeaderMap,
+    mut mp: Multipart,
+) -> Result<Response, Error> {
     let config = CONFIG.get().context("couldn't get config")?;
     let bucket = BUCKET.get().context("couldn't get bucket")?.clone();
-    let mut meta: Option<UploadMetadata> = None;
-    loop {
-        let field = match mp.next_field().await {
-            Ok(Some(field)) => field,
-            Ok(None) => break,
-            Err(_) => {
-                return Response::builder()
-                    .status(StatusCode::BAD_REQUEST)
-                    .body(Body::empty())
-                    .context("Error getting next field");
-            }
-        };
 
-        let name = match field.name() {
-            Some(x) => x.to_string(),
-            None => {
-                return Response::builder()
-                    .status(StatusCode::BAD_REQUEST)
-                    .body(Body::empty())
-                    .context(
-                        "error parsing unnamed multipart/form-data part",
-                    );
-            }
-        };
-
-        if name == "metadata" {
-            if meta.is_some() {
-                bail!("multiple metadata fields in multipart/form-data")
-            }
-            let data = match field.bytes().await {
+    let signing_key = {
+        let auth = match headers.get("Authorization") {
+            Some(x) => match x.to_str() {
                 Ok(x) => x,
                 Err(_) => {
                     return Response::builder()
                         .status(StatusCode::BAD_REQUEST)
                         .body(Body::empty())
-                        .context(
-                            "error getting multipart/form-data field content",
-                        );
+                        .context("error creating request");
                 }
-            };
+            },
+            None => {
+                return Response::builder()
+                    .status(StatusCode::BAD_REQUEST)
+                    .body(Body::empty())
+                    .context("error creating request");
+            }
+        };
+        // TODO: get signing key from this
+    };
 
-            let um = serde_json::from_slice::<UploadMetadata>(&data)
-                .context("error parsing metadata as JSON")?;
-            meta = Some(um);
-        } else if name == "file" {
-            bucket
-                .put_object_stream(
-                    &mut StreamReader::new(field),
-                    format!("cool"),
-                )
-                .await;
-            return Response::builder()
-                .body(Body::from_stream(field))
-                .context("couldn't send body as response");
-        } else {
+    let field = if let Ok(Some(x)) = mp.next_field().await {
+        x
+    } else {
+        return Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(Body::empty())
+            .context("error creating request");
+    };
+    let name = match field.name() {
+        Some(x) => x,
+        None => {
             return Response::builder()
                 .status(StatusCode::BAD_REQUEST)
                 .body(Body::empty())
-                .context("unexpected multipart/form-data field");
+                .context("error creating request");
         }
+    };
+
+    if name != "metadata" {
+        return Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(Body::empty())
+            .context("error creating request");
     }
-    unreachable!()
+
+    let bytes = field
+        .bytes()
+        .await
+        .context("error getting data for metadata")?;
+    let um = serde_json::from_slice::<UploadMetadata>(&bytes)
+        .context("error parsing metadata as JSON")?;
+
+    let field = if let Ok(Some(x)) = mp.next_field().await {
+        x
+    } else {
+        return Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(Body::empty())
+            .context("error creating request");
+    };
+    let name = match field.name() {
+        Some(x) => x,
+        None => {
+            return Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::empty())
+                .context("error creating request");
+        }
+    };
+
+    if name != "data" {
+        return Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(Body::empty())
+            .context("error creating request");
+    }
 }
